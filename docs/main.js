a1b870972b1bc9a15574f45800278f96168e3af6
ThatGuy2028 <briancolantuoni06@gmail.com>
2022-03-24 18:43:03 -0400

    front end mostly done

--- docs/main.js
+++ docs/main.js
@@ -2,14 +2,23 @@ let fname;
 let lname;
 let belt;
 let question;
-let answer;
+let option1;
+let option2;
+let dateObj = new Date();
+let date = `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`
 const FORMDIV = document.getElementById("form");
 
 function submitAction() {
     fname = document.getElementById("fname").value;
     lname = document.getElementById("lname").value;
     belt = document.getElementById("belt").value;
+    answer = document.getElementById("answer").value;
 
-    console.log(fname + " " + lname + " " + belt);
+    console.log(fname + " " + lname + " " + belt + "" + answer);
 }
 
+window.onload = function() {
+    document.getElementById("option1").innerHTML = option1;
+    document.getElementById("option2").innerHTML = option2;
+    document.getElementById("date").innerHTML = date;
+}
