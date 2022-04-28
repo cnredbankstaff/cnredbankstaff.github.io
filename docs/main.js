19bb70a9ab311f52b8a0ee1273fe95757d187a07
ThatGuy2028 <briancolantuoni06@gmail.com>
2022-04-28 19:07:25 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -163,7 +163,7 @@ window.onload = function() {
     document.getElementById("date").innerHTML = date;
     var interval = setInterval(function(){
       if(answered === true) {
-        doneText.display = "initial";
+        doneText.display = "block";
         console.log("also worked");
         clearInterval(interval);
       }
