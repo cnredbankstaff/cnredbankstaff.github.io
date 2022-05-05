0f2a0e90c8dd04989fbf9546a4f4eb0a717ffc7d
ThatGuy2028 <briancolantuoni06@gmail.com>
2022-05-05 16:27:28 -0400

    maybe fixed

--- docs/main.js
+++ docs/main.js
@@ -163,7 +163,7 @@ window.onload = function() {
     document.getElementById("date").innerHTML = date;
     var interval = setInterval(function(){
       if(answered === true) {
-        doneText.display = "block";
+        doneText.visiblity = "visible";
         console.log("also worked");
         clearInterval(interval);
       }
